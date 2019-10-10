4275988ba55b616640e784d8c1f2bada346fcbca
carmelp2rn <44434795+carmelp2rn@users.noreply.github.com>
2019-10-10 22:10:09 +0300

    Update script.js

---
+++ res/js/script.js
@@ -0,0 +1,6 @@
+$(function () {
+    var user = new User("Will", "Smith", 11/11/1970, "Computer Science", 2.80);
+    var course = new Course(["Web Application Development", "Programming", "Mathematics", "Multimedia" ], [4, 3, 2, 1],
+        [85, 90, 75, 60]);
+
+})
