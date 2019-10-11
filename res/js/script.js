ff21e4cef4d558465a6580983b822a0133adf890
kristinalillo <kristina.lillo@gmail.com>
2019-10-12 02:14:23 +0300

    Task 2 fixed

--- res/js/script.js
+++ res/js/script.js
@@ -1,9 +1,13 @@
 $(function () {
-    var user = new User("Will", "Smith", 11/11/1970, "Computer Science", 2.80);
-    var course = new Course(["Web Application Development", "Programming", "Mathematics", "Multimedia" ], [4, 3, 2, 1],
-        [85, 90, 75, 60]);
+    var user = new User("Will", "Smith", "11/11/1970", "Computer Science", 0);
+    var courses = [
+        new Course("Web Application Development", 2, 85),
+        new Course("Programming", 2, 90),
+        new Course("Mathematics", 3, 75),
+        new Course("Multimedia", 3, 60)
+    ];
+    init();
 
-    //Showing courses page
     $("#courses-button").click(function() {
         $("#profile").hide();
         $("#courses-container").show()
@@ -11,7 +15,7 @@ $(function () {
         $('#profile-button').removeClass('pill active').addClass('pill');
 
     });
-    //Showing profile page
+
     $("#profile-button").click(function () {
         $("#courses-container").hide();
         $("#profile").show()
@@ -20,10 +24,37 @@ $(function () {
 
     });
 
-    //Adding courses blue-button
     $("#add-course-button").click(function () {
         $("#add-course").toggle();
     });
 
 
+function init() {
+
+    $(".info #name").text(user.firstname + " " + user.lastname);
+    $(".info #birthdate").text(user.birthdate);
+    $(".info #faculty").text(user.faculty);
+    $("#profile #gpa strong").text(user.gpa);
+
+    for (let i = 0; i < courses.length; i++) {
+        let tr = $("<tr></tr>");
+        let nr = $("<td></td>");
+        let title = $("<td></td>");
+        let semester = $("<td></td>");
+        let grade = $("<td></td>");
+
+        let id = parseInt($("#courses tbody tr:last td:first").text()) + 1;
+
+        nr.text(id);
+        title.text(courses[i].title);
+        semester.text(courses[i].semester);
+        grade.text(courses[i].grade);
+
+        tr.append(nr, title, semester, grade);
+
+        $("#courses tr:last").after(tr);
+    }
+}
+
 });
+
