d2f9f0bdbca540a9f16c83712161a72361cbd5b9
kristinalillo <kristina.lillo@gmail.com>
2019-10-12 01:48:37 +0300

    Task 4 done

--- res/js/script.js
+++ res/js/script.js
@@ -3,24 +3,27 @@ $(function () {
     var course = new Course(["Web Application Development", "Programming", "Mathematics", "Multimedia" ], [4, 3, 2, 1],
         [85, 90, 75, 60]);
 
-
+    //Showing courses page
     $("#courses-button").click(function() {
         $("#profile").hide();
         $("#courses-container").show()
         $(this).removeClass('pill').addClass('pill active')
         $('#profile-button').removeClass('pill active').addClass('pill');
 
-    })
-
+    });
+    //Showing profile page
     $("#profile-button").click(function () {
         $("#courses-container").hide();
         $("#profile").show()
         $(this).removeClass('pill').addClass('pill active')
         $('#courses-button').removeClass('pill active').addClass('pill');
 
+    });
 
-    })
-
+    //Adding courses blue-button
+    $("#add-course-button").click(function () {
+        $("#add-course").toggle();
+    });
 
 
 });
